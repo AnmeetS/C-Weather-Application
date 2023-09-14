f07525b261c6e27ae18e64a1d7f5f848e9b0bc43
Anmeet Sekhon <as2sekho@uwaterloo.ca>
2023-09-13 23:52:04 -0400

    V0.1: Basic classes have been created

--- C# Weather Application/Form1.cs
+++ C# Weather Application/Form1.cs
@@ -16,5 +16,10 @@ namespace C__Weather_Application
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
